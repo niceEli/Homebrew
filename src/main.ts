2d4912cade1a7a9678934aac6662083e480235c1
Eli <78963198+niceEli@users.noreply.github.com>
2023-11-03 14:00:43 +0000

    Jillian did something

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,17 @@ import k from './kaboom';
 // Import Scenes
 import HelloWorld from './scenes/helloworld';
 import kMatterTest from './scenes/kMatterTest';
+import Jilian from './scenes/Jillian';
 
 // Add Scenes
 k.scene('Hello-World-EX', HelloWorld)
 k.scene('kMatter-Test', kMatterTest)
+k.scene('Jillian-Test-Scene', Jilian)
 
 // Run Scenes
 
 //k.go('Hello-World-EX'); // This is a template scene
 
-k.go('kMatter-Test') // This is a physics (matter) template scene
+//k.go('kMatter-Test') // This is a physics (matter) template scene
+
+k.go('Jillian-Test-Scene')
