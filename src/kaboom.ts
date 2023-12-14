2151def8f9d2a0e0108ad8d9e8d5aaae8c90c0d6
Eli <78963198+niceEli@users.noreply.github.com>
2023-12-14 14:53:55 -0500

    make even better

--- src/kaboom.ts
+++ src/kaboom.ts
@@ -1,7 +1,9 @@
 import kaboom from "kaboom";
+import flexUIPlugin from "kaboom-flex-ui";
 
 export const k = kaboom({
   background: [255, 255, 255],
+  plugins: [flexUIPlugin]
 });
 
 export default k;
