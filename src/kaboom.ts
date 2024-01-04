67e057715288a4ad5663aeb5d985e4f1aabe426b
Eli <78963198+niceEli@users.noreply.github.com>
2024-01-04 12:11:21 -0500

    Bugfix PT2

--- src/kaboom.ts
+++ src/kaboom.ts
@@ -1,9 +1,7 @@
 import kaboom from "kaboom";
-import flexUIPlugin from "kaboom-flex-ui";
 
 export const k = kaboom({
   background: [255, 255, 255],
-  plugins: [flexUIPlugin]
 });
 
 export default k;
