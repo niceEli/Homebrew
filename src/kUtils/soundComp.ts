937ab2cdb65175b45c8b8f274b9147956709efb6
Eli <78963198+niceEli@users.noreply.github.com>
2024-02-19 14:51:04 -0500

    Create soundComp.ts

---
+++ src/kUtils/soundComp.ts
@@ -0,0 +1,10 @@
+import { Asset, AudioPlay, AudioPlayOpt, SoundData } from "kaboom";
+import k from "../kaboom";
+
+export default function soundComp(
+  src: string | SoundData | Asset<SoundData>,
+  options?: AudioPlayOpt
+): AudioPlay {
+  let sound = k.play(src, options);
+  return sound;
+}
