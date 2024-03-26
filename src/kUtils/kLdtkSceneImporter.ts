64619c078be6690f6bfc5df7667249b04ba79698
Eli <78963198+niceEli@users.noreply.github.com>
2024-03-25 21:19:33 -0400

    player exists in global now

--- src/kUtils/kLdtkSceneImporter.ts
+++ src/kUtils/kLdtkSceneImporter.ts
@@ -180,6 +180,7 @@ export default function kLdtkSceneImporter(
               "Player",
               "Solid",
             ]);
+            globalThis.player = player;
           }
         }
       }
